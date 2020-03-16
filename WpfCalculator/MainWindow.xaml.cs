dd303442b24bb5b85124c68c31a5e14863ef06a2
Strange.Sub <Strange.Sub@mail.ru>
2020-03-16 18:45:55 +0500

    StackPanel, interface, delete button

--- WpfCalculator/MainWindow.xaml.cs
+++ WpfCalculator/MainWindow.xaml.cs
@@ -21,41 +21,73 @@ namespace WpfCalculator
     public partial class MainWindow : Window
     {
         int result = 0;
+        int coordinate = 5;
         public MainWindow()
         {
             InitializeComponent();
             label_res.Content = result;
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+        public void Button_Click(object sender, RoutedEventArgs e)
         {
             TextBox textBox = new TextBox()
             {
-                Width = 120,
-                Height = 40,
-                Margin = new Thickness(10, 0, 0, 0)
+                Margin = new Thickness(5, coordinate, 5, 5)
 
             };
 
             textBox.TextChanged += Text_Changed;
+            coordinate += 1;
+            mySP.Children.Add(textBox);
 
-            myGrid.Children.Add(textBox);
+            //ComboBox operations = OperComboBox();
+
         }
 
-        private void Text_Changed(object sender, RoutedEventArgs e)
+        //private ComboBox OperComboBox()
+        //{
+        //    ComboBox CB = new ComboBox();
+        //    //foreach (TextBox textBox in )
+        //    {
+
+        //        < ComboBox CB = "Operations" >
+        //        {
+        //        < TextBlock > + </ TextBlock >
+        //        < TextBlock > - </ TextBlock >
+        //        < TextBlock > * </ TextBlock >
+        //        < TextBlock > / </ TextBlock >
+        //        }
+        //    </ ComboBox >
+
+        //    }
+        //    return CB;
+        //}
+
+
+        public void Text_Changed(object sender, RoutedEventArgs e)
         {
             int sum = 0;
-            foreach (var element in myGrid.Children)
+            foreach (var element in mySP.Children)
             {
                 if (element is TextBox text)
                 {
                     if (int.TryParse(text.Text, out var num)) ;
-                    else
-                        MessageBox.Show("Это не число. Введи нормально.");
+
                     sum += num;
                 }
             }
             label_res.Content = sum;
         }
 
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            int spLength = mySP.Children.Count - 1;
+            for (int i = spLength; i >= 0; i--)
+            {
+                if (mySP.Children[i] is TextBox)
+                    mySP.Children.RemoveAt(i);
+                break;
+            }
+            Text_Changed(sender, e);
+        }
     }
 }
